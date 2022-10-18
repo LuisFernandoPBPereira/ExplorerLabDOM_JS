c131dad7c75a7817e993aecc23a90b5935caa64f
LuisFernandoPBPereira <luisfernandopbp@gmail.com>
2022-10-17 23:06:35 -0300

    adding dom manipulation with colors and svg's

--- src/main.js
+++ src/main.js
@@ -1,1 +1,20 @@
 import "./css/index.css"
+
+const bgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
+const bgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
+
+const logo = document.querySelector(".cc-logo span:nth-child(2) img")
+
+function setCardType(type){
+    const cardColors = {
+        visa: ["#436D99", "#2D57F2"],
+        mastercard: ["#DF6F29", "#C69347"],
+        default: ["black", "gray"],
+    }
+
+    bgColor01.setAttribute("fill", cardColors[type][0])
+    bgColor02.setAttribute("fill", cardColors[type][1])
+    logo.setAttribute("src", `cc-${type}.svg`)
+}
+
+setCardType("default")
